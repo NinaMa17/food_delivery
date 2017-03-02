a45ede109072d1596fcf6d0e64757f70ccc7d4e9
NinaMa17 <xma008@e.ntu.edu.sg>
2017-03-02 18:21:42 +0800

    Add figure caption
    Still editing.
    Cannot put 3 images in the same row.

--- WEB-INF/classes/FoodDisplay.java
+++ WEB-INF/classes/FoodDisplay.java
@@ -29,7 +29,7 @@ public class FoodDisplay extends HttpServlet {  // JDK 6 and above only
          stmt = conn.createStatement();
  
          // Step 3: Execute a SQL SELECT query
-         String sqlStr = "SELECT I.img_name, F.food_name, F.canteen_id FROM food F, food_img I where F.food_id=I.food_id ORDER BY canteen_id ASC";
+         String sqlStr = "SELECT I.img_name, F.food_name, F.food_id, F.canteen_id FROM food F, food_img I where F.food_id=I.food_id ORDER BY canteen_id ASC";
          ResultSet rset = stmt.executeQuery(sqlStr); // Send the query to the server
          
          // Print an HTML page as output of query
@@ -43,16 +43,19 @@ public class FoodDisplay extends HttpServlet {  // JDK 6 and above only
              if (canteen != temp) {
                  temp = canteen;
                  out.println("<hr>");
-                 out.println("<p><center><font color='white'>Canteen" + canteen + "</font></center></p>");
+                 out.println("<p><center><font color='white' size='200'>Canteen" + canteen + "</font></center></p>");
                  align_count = 1;
              }
              if (align_count%3==1) {
                  //out.println("<p align='left'>");
-                 out.println("<span style='text-align:left;'><img src='"+rset.getString("I.img_name")+"' alt='"+ rset.getString("F.food_name")+"' style='width:320px;height:240px;' /></span>");
+                 //out.println("<span style='text-align:left;'><img src='"+rset.getString("I.img_name")+"' alt='"+ rset.getString("F.food_name")+"' style='width:320px;height:240px;' /></span>");
+                 out.println("<span style='text-align:left;'><figure style='text-align:left;width:33%;margin:0px;padding:0px;'><img src='"+rset.getString("I.img_name")+"' alt='"+ rset.getString("F.food_name")+"' style='width:320px;height:240px;' />");
+                 out.println("<figcaption style='text-align:center;width:320px;'><input type='checkbox' name='food_id' value='"+rset.getInt("F.food_id")+"' />"+rset.getString("F.food_name")+"</figcaption></figure></span>");
              }
              if (align_count%3==2) {
                  //out.println("<p align='center'>");
-                 out.println("<span style='text-align:center;'><img src='"+rset.getString("I.img_name")+"' alt='"+ rset.getString("F.food_name")+"' style='width:320px;height:240px;' /></span>");
+                 out.println("<span style='text-align:center;'><figure style='text-align:center;width:33%;'><img src='"+rset.getString("I.img_name")+"' alt='"+ rset.getString("F.food_name")+"' style='width:320px;height:240px;' />");
+                 out.println("<figcaption style='text-align:center;width:320px;'><input type='checkbox' name='food_id' value='"+rset.getInt("F.food_id")+"' />"+rset.getString("F.food_name")+"</figcaption></figure></span>");
              }
              if (align_count%3==0) {
                  //out.println("<p align='right'>");
